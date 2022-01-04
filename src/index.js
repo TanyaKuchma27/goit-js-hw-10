cef6b955af484f25c6d13fec8982ba3a5ab22512
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-01-04 13:08:45 +0200

    change name.common to name.official

--- src/index.js
+++ src/index.js
@@ -34,28 +34,31 @@ function onInput(evt) {
                 renderCountries(names);
             }
         })
-    .catch((error) => console.log(error));
+        .catch(error => console.log(error));
 };
 
 function renderCountry(names) {
-    const markup = names.map(name => {
-    return `<div class="country-item">
-        <img class="flag" src="${name.flags.svg}" alt="flag of ${name.name.common}" width="30"> 
-        <p class="title-big">${name.name.common}</p>
-        </div>
-        <p><span class="list-text">Capital: </span>${name.capital}</p>
-        <p><span class="list-text">Population: </span>${name.population}</p>
-        <p><span class="list-text">Languages: </span>${Object.values(name.languages)} </p>`;
-    });
-    refs.countryInfo.innerHTML = markup; 
+    const markup = names
+        .map(({ flags, name, capital, population, languages }) => {    
+            return `<div class="country-item">
+                <img class="flag" src="${flags.svg}" alt="flag of ${name.official}" width="30"> 
+                <p class="title-big">${name.official}</p>
+                </div>
+                <p><span class="list-text">Capital: </span>${capital}</p>
+                <p><span class="list-text">Population: </span>${population}</p>
+                <p><span class="list-text">Languages: </span>${Object.values(languages)} </p>`;
+            })
+        .join("");
+    
+    refs.countryInfo.innerHTML = markup;  
 };
 
 function renderCountries(names) {
     const markup = names
-        .map(name => {
+        .map(({flags, name}) => {
             return `<li class="country-item">
-            <img class="flag" src="${name.flags.svg}" alt="flag of ${name.name.common}" width="30"> 
-            <p class="title">${name.name.common}</p>           
+            <img class="flag" src="${flags.svg}" alt="flag of ${name.official}" width="30"> 
+            <p class="title">${name.official}</p>           
             </li>`;
         })
         .join("");
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 export function fetchCountries(name) {
     const searchParams = 'name,capital,population,flags,languages';
-    const http = `https://restcountries.com/v3.1/name/${name}?fields=${searchParams}`;
-    return fetch(http)
-    .then(response => response.json())
+    const https = `https://restcountries.com/v3.1/name/${name}?fields=${searchParams}`;
+    return fetch(https)
+        .then(response => response.json());
 } 
